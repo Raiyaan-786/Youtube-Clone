26aa52dd3c527ccb4998468b3e1e09bc0f35bb8c
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2023-10-07 18:50:22 +0530

    R- Added react router dom , and linked home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import './App.css'
-function App() {
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
 
+import Navbar from './Components/Navbar/Navbar';
+import Home from './Pages/Home/Home';
+
+function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar/>
+        <Outlet/>
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      children: [
+        {
+          path: "/",
+          element: <Home/>
+        },
+      ]
+    }
+  ])
   return (
-    <div className='App'>
-      App inzamam ul haque
+    <div className='app'>
+      <RouterProvider router={router}/>
     </div>
   )
 }
