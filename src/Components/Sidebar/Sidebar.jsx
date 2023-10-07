ba93a1c13f8fc32e77121689fc911db40139566d
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2023-10-07 19:58:07 +0530

    R- Sidebar Little work done

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,8 +1,57 @@
 import React from 'react'
 import './Sidebar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay, faHouse, faMoneyBill } from '@fortawesome/free-solid-svg-icons'
+
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='sidebar-container'>
+      <div className="sidebar-section">
+        <div className='sidebar-content'>
+          <FontAwesomeIcon icon={faHouse}  className='icons'/>
+          Home
+        </div>
+        <div className='sidebar-content'>
+          <FontAwesomeIcon icon={faCirclePlay}  className='icons'/>
+          Shorts
+        </div>
+        <div className='sidebar-content'>
+          <FontAwesomeIcon icon={faMoneyBill} className='icons' />
+          Subscription
+        </div>
+      </div>
+      <hr />
+      <div className="sidebar-section">
+        <div className='sidebar-content'>
+          <FontAwesomeIcon icon={faHouse}  className='icons'/>
+          Library
+        </div>
+        <div className='sidebar-content'>
+          <FontAwesomeIcon icon={faCirclePlay}  className='icons'/>
+          History
+        </div>
+        <div className='sidebar-content'>
+          <FontAwesomeIcon icon={faMoneyBill} className='icons' />
+          Your Videos
+        </div>
+        <div className='sidebar-content'>
+          <FontAwesomeIcon icon={faMoneyBill} className='icons' />
+          Watch Later 
+        </div>
+        <div className='sidebar-content'>
+          <FontAwesomeIcon icon={faMoneyBill} className='icons' />
+          Liked Videos
+        </div>
+        
+      </div>
+      <hr />
+      <div className="sidebar-section"></div>
+      <hr />
+      <div className="sidebar-section"></div>
+      <div className="sidebar-section"></div>
+      <div className="sidebar-section"></div>
+      <div className="sidebar-section"></div>
+    </div>
   )
 }
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import './Home.css'
+import Sidebar from '../../Components/Sidebar/Sidebar'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home-container'>
+        <div className="home-left">
+            <Sidebar/>
+        </div>
+        <div className="home-right">
+            Home
+        </div>
+    </div>
   )
 }
 
