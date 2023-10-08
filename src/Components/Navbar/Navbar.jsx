58a1abdc09800bb3734103ad8dcdac1eeb213032
Inzamam-Ul Haque <132208469+InzamamulHaque0786@users.noreply.github.com>
2023-10-08 14:38:50 +0530

    I-navbar work half done

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faBell,
+  faMicrophone,
+  faSearch,
+  faVideoCamera,
+} from "@fortawesome/free-solid-svg-icons";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav>
+        <div className="bar-logo">
+          <div className="nicon">
+            <FontAwesomeIcon icon={faBars} size="lg" />
+          </div>
+          <div className="logo">
+            <img src="logo/logo-light-mode1.png" alt="logo" />
+          </div>
+        </div>
 
-export default Navbar
+
+        <div className="search-bar">
+
+          <div className="search-box">
+            <input type="text" placeholder="Search" />
+            <button>
+              <FontAwesomeIcon icon={faSearch} size="lg" />
+            </button>
+          </div>
+
+          <div className="nicon" id="mic">
+            <FontAwesomeIcon icon={faMicrophone} size="lg" />
+          </div>
+
+        </div>
+
+
+
+        <div id="user">
+          <div className="nicon">
+            <FontAwesomeIcon icon={faVideoCamera} size="lg" />
+          </div>
+          <div className="nicon">
+            <FontAwesomeIcon icon={faBell} size="lg" />
+          </div>
+          <div className="nicon">
+            <img src="logo/profile.jpg" alt="" />
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
