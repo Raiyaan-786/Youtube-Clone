f2d044e4962efb8b618664084b2bc8820655c92e
Inzamam-Ul Haque <132208469+InzamamulHaque0786@users.noreply.github.com>
2023-12-09 17:54:36 +0530

    slider-feed component basic done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './Home.css'
 import DefaultSidebar from '../../Components/DefaultSidebar/DefaultSidebar'
+import SliderFeed from '../../components/SliderFeed/SliderFeed'
 const Home = () => {
   return (
     <div className='home-container'>
         <div className="home-left">
           <DefaultSidebar/>
         </div>
+        
         <div className="home-right">
-            Home
+        <SliderFeed/>
+            home
         </div>
+        
     </div>
   )
 }
-
 export default Home
