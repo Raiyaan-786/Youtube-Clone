66fb35c7beb5061b4b13dfdc27965b6fc73108a7
Raiyaan-786 <120575869+Raiyaan-786@users.noreply.github.com>
2023-12-21 22:21:42 +0530

    R - Sidebar functionality added

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,54 +1,144 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown, faCirclePlay, faCircleQuestion, faClapperboard, faClock, faClockRotateLeft, faFilm, faFire, faFlag, faGamepad, faGear, faGraduationCap, faHeadphones, faHouse, faMoneyBill, faMusic, faNewspaper, faPaperPlane, faPhotoFilm, faPodcast, faRss, faThumbsUp, faTrophy, faTv, faVestPatches, faVideo } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faAngleLeft, faAngleRight, faAngleUp, faCirclePlay, faCircleQuestion, faClapperboard, faClock, faClockRotateLeft, faFilm, faFire, faFlag, faGamepad, faGear, faGraduationCap, faHeadphones, faHouse, faMoneyBill, faMusic, faNewspaper, faPaperPlane, faPhotoFilm, faPodcast, faRss, faSliders, faThumbsUp, faTrophy, faTv, faVestPatches, faVideo } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = ({openSidebar}) => {
+  const [OpenShowMore , setOpenShowMore] = useState(false);
+  const [OpenShowSubs , setOpenShowSubs] = useState(false);
+  const ShowMoreHandler = () => {
+    setOpenShowMore(!OpenShowMore);
+  }
+  const ShowSubsHandler = () => {
+    setOpenShowSubs(!OpenShowSubs);
+  }
   return (
     <div>
     <div className={openSidebar?"sidebar-container sidebar-collapse":"sidebar-container "}>
       <div className="sidebar-section">
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faHouse} className='icons'/>
-          Home
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faHouse} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Home
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faCirclePlay} className='icons'/>
-          Shorts
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faCirclePlay} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Shorts
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faMoneyBill} className='icons'/>
-          Subscription
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faMoneyBill} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Subscription
+          </div>
         </div>
       </div>
 
       <hr />
 
       <div className="sidebar-section">
+      <span>You <FontAwesomeIcon icon={faAngleRight} className='icons'/></span>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faPhotoFilm} className='icons'/>
-          Library
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faPhotoFilm} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Library
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faClockRotateLeft} className='icons'/>
-          History
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faClockRotateLeft} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            History
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faVideo} className='icons'/>
-          Your Videos
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faVideo} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Your Videos
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faClock} className='icons'/>
-          Watch Later 
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faClock} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Watch Later
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faThumbsUp} className='icons'/>
-          Liked Videos
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faThumbsUp} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Liked Videos
+          </div>
+        </div>
+
+        {!OpenShowMore && <div className='sidebar-content' onClick={() => ShowMoreHandler()}>
+          <div className='sidebar-content-icon' >
+            <FontAwesomeIcon icon={faAngleDown} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Show More
+          </div>
+        </div>}
+        
+        {OpenShowMore && 
+        <>
+        <div className='sidebar-content'>
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faSliders} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Relax
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faAngleDown} className='icons'/>
-          Show more
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faSliders} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Pubg Montage
+          </div>
         </div>
+        <div className='sidebar-content'>
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faSliders} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            20 mins Workout
+          </div>
+        </div>
+        <div className='sidebar-content'>
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faSliders} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Best Songs 2023
+          </div>
+        </div>
+        <div className='sidebar-content' onClick={() => ShowMoreHandler()}>
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faAngleUp} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Show Fewer
+          </div>
+        </div>
+        </>}
       </div>
 
       <hr/>
@@ -56,29 +146,82 @@ const Sidebar = ({openSidebar}) => {
       <div className="sidebar-section">
         <span>Subscription</span>
         <div className='sidebar-content'>
-          <img src="https://images.pexels.com/photos/3693108/pexels-photo-3693108.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-          T-Series
+          <div className='sidebar-content-img'>
+            <img src="https://images.pexels.com/photos/3693108/pexels-photo-3693108.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+          <div className='sidebar-content-text'>
+            T-Series
+          </div>
         </div>
         <div className='sidebar-content'>
-          <img src="https://images.pexels.com/photos/18482086/pexels-photo-18482086/free-photo-of-landscape-wallpaper-valley-and-mountains-in-autumn.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-          YRF Films
+          <div className='sidebar-content-img'>
+            <img src="https://images.pexels.com/photos/18482086/pexels-photo-18482086/free-photo-of-landscape-wallpaper-valley-and-mountains-in-autumn.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+          <div className='sidebar-content-text'>
+            YRF Films
+          </div>
         </div>
         <div className='sidebar-content'>
-          <img src="https://images.pexels.com/photos/18446247/pexels-photo-18446247/free-photo-of-young-man-in-sunglasses-posing-near-handrails.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-          Dhruv Rathee
+          <div className='sidebar-content-img'>
+            <img src="https://images.pexels.com/photos/18446247/pexels-photo-18446247/free-photo-of-young-man-in-sunglasses-posing-near-handrails.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+          <div className='sidebar-content-text'>
+            Dhruv Rathee
+          </div>
         </div>
         <div className='sidebar-content'>
-          <img src="https://images.pexels.com/photos/3194495/pexels-photo-3194495.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-          Red Chillies
+          <div className='sidebar-content-img'>
+            <img src="https://images.pexels.com/photos/3194495/pexels-photo-3194495.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+          <div className='sidebar-content-text'>
+            Red Chillies
+          </div>
         </div>
         <div className='sidebar-content'>
-          <img src="https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-          Mr Beast
+          <div className='sidebar-content-img'>
+            <img src="https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+          <div className='sidebar-content-text'>
+            Mr Beast
+          </div>
+        </div>
+        {!OpenShowSubs && <div className='sidebar-content' onClick={() => ShowSubsHandler()}>
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faAngleDown} className='icons sicons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Show More
+          </div>
+        </div>}
+
+        {OpenShowSubs &&
+          <>
+          <div className='sidebar-content'>
+          <div className='sidebar-content-img'>
+            <img src="https://images.pexels.com/photos/14468172/pexels-photo-14468172.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+          <div className='sidebar-content-text'>
+            Nature
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faAngleDown} className='icons sicons'/>
-          Show more
+          <div className='sidebar-content-img'>
+            <img src="https://images.pexels.com/photos/19554104/pexels-photo-19554104/free-photo-of-a-man-taking-a-photo-of-the-sunset-on-the-beach.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+          </div>
+          <div className='sidebar-content-text'>
+            Photomania
+          </div>
         </div>
+        <div className='sidebar-content' onClick={() => ShowSubsHandler()}>
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faAngleUp} className='icons sicons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Show Fewer
+          </div>
+        </div>
+        </>
+        }
       </div>
 
       <hr />
@@ -86,48 +229,92 @@ const Sidebar = ({openSidebar}) => {
       <div className="sidebar-section">
       <span>Explore</span>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faFire}  className='icons'/>
-          Trending
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faFire} className='icons sicons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Trending
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faCirclePlay}  className='icons'/>
-          Shopping
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faCirclePlay} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Shopping
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faHeadphones} className='icons' />
-          Music
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faHeadphones} className='icons' />
+          </div>
+          <div className='sidebar-content-text'>
+            Music
+          </div>          
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faFilm} className='icons' />
-          Films
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faFilm} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Films
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faRss} className='icons' />
-          Live 
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faRss} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Live
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faGamepad} className='icons' />
-          Gaming 
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faGamepad} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Gaming
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faNewspaper} className='icons' />
-          News 
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faNewspaper} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            News 
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faTrophy} className='icons' />
-          Sport 
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faTrophy} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Sport
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faGraduationCap} className='icons' />
-          Learing
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faGraduationCap} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Learning
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faVestPatches} className='icons' />
-          Fashion & beauty
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faVestPatches} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Fashion & beauty
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faPodcast} className='icons' />
-          Podcast
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faPodcast} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Podcast
+          </div>
         </div>
       </div>
 
@@ -136,20 +323,36 @@ const Sidebar = ({openSidebar}) => {
       <div className="sidebar-section">
         <span>More from Youtube</span>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faCirclePlay}  className='icons col-icons'/>
-          Youtube Premium
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faCirclePlay}  className='icons col-icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Youtube Premium
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faTv}  className='icons col-icons'/>
-          Youtube Studio
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faTv}  className='icons col-icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Youtube Studio
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faMusic} className='icons col-icons' />
-          Youtube Music
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faMusic}  className='icons col-icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Youtube Music
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faClapperboard} className='icons col-icons' />
-          Youtube Kids
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faClapperboard}  className='icons col-icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Youtube Kids
+          </div>
         </div>
       </div>
 
@@ -157,20 +360,36 @@ const Sidebar = ({openSidebar}) => {
 
       <div className="sidebar-section">
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faGear}  className='icons'/>
-          Settings
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faGear} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Settings
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faFlag}  className='icons'/>
-          Report history
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faFlag} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Report history
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faCircleQuestion} className='icons' />
-          Help 
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faCircleQuestion} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Help
+          </div>
         </div>
         <div className='sidebar-content'>
-          <FontAwesomeIcon icon={faPaperPlane} className='icons' />
-          Send Feedback
+          <div className='sidebar-content-icon'>
+            <FontAwesomeIcon icon={faPaperPlane} className='icons'/>
+          </div>
+          <div className='sidebar-content-text'>
+            Send Feedback
+          </div>
         </div>
       </div>
 
